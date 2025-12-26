d197bee0ddd299b2ecd7eef18d0fa91d1e5e492b
gunzblazincourier <gunzblazinAKM@proton.me>
2025-12-25 22:15:29 -0700

    Add simultaneous sound playback to load 2 .wav files at once

--- pong.cpp
+++ pong.cpp
@@ -9,11 +9,11 @@
 #define SDL_MAIN_USE_CALLBACKS 1    /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-#include <iostream>
 
 /* We will use this renderer to draw into this window every frame. */
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
+static SDL_AudioDeviceID audio_device = 0;
 static Uint64 last_time = 0;    // The time deltatime before the current frame
 
 static const int WINDOW_WIDTH = 640;
@@ -39,12 +39,50 @@ static float s_component_ball_x = SDL_randf();
 static int s_score_player = 0;
 static int s_score_cpu = 0;
 
+/* things that are playing sound (the audiostream itself, plus the original data, so we can refill to loop. */
+typedef struct Sound {
+    Uint8 *wav_data;
+    Uint32 wav_data_len;
+    SDL_AudioStream *stream;
+} Sound;
+
+static Sound sounds[2];
+
+static bool init_sound(const char *fname, Sound *sound)
+{
+    bool retval = false;
+    SDL_AudioSpec spec;
+    char *wav_path = NULL;
+
+    /* Load the .wav files from wherever the app is being run from. */
+    SDL_asprintf(&wav_path, "%s%s", SDL_GetBasePath(), fname);  /* allocate a string of the full file path */
+    if (!SDL_LoadWAV(wav_path, &spec, &sound->wav_data, &sound->wav_data_len)) {
+        SDL_Log("Couldn't load .wav file: %s", SDL_GetError());
+        return false;
+    }
+
+    /* Create an audio stream. Set the source format to the wav's format (what
+       we'll input), leave the dest format NULL here (it'll change to what the
+       device wants once we bind it). */
+    sound->stream = SDL_CreateAudioStream(&spec, NULL);
+    if (!sound->stream) {
+        SDL_Log("Couldn't create audio stream: %s", SDL_GetError());
+    } else if (!SDL_BindAudioStream(audio_device, sound->stream)) {  /* once bound, it'll start playing when there is data available! */
+        SDL_Log("Failed to bind '%s' stream to device: %s", fname, SDL_GetError());
+    } else {
+        retval = true;  /* success! */
+    }
+
+    SDL_free(wav_path);  /* done with this string. */
+    return retval;
+}
+
 /* This function runs once at startup */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     SDL_SetAppMetadata("Pong", "1.0", "gunz-sdl3-pong");
 
     // We will use this renderer to draw into this window every frame
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)) {
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
@@ -55,6 +93,19 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     }
     /* Set a device-independent resolution and presentation mode for rendering. */
     SDL_SetRenderLogicalPresentation(renderer, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_LOGICAL_PRESENTATION_INTEGER_SCALE);
+
+    /* open the default audio device in whatever format it prefers; our audio streams will adjust to it. */
+    audio_device = SDL_OpenAudioDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, NULL);
+    if (audio_device == 0) {
+        SDL_Log("Couldn't open audio device: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!init_sound("sample.wav", &sounds[0])) {
+        return SDL_APP_FAILURE;
+    } else if (!init_sound("sample1.wav", &sounds[1])) {
+        return SDL_APP_FAILURE;
+    }
 
     return SDL_APP_CONTINUE;
 }
@@ -93,6 +144,16 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 /* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
+    int i;
+
+    for (i = 0; i < SDL_arraysize(sounds); i++) {
+        /* If less than a full copy of the audio is queued for playback, put another copy in there.
+           This is overkill, but easy when lots of RAM is cheap. One could be more careful and
+           queue less at a time, as long as the stream doesn't run dry.  */
+        if (SDL_GetAudioStreamQueued(sounds[i].stream) < ((int) sounds[i].wav_data_len)) {
+            SDL_PutAudioStreamData(sounds[i].stream, sounds[i].wav_data, (int) sounds[i].wav_data_len);
+        }
+    }
     /* Get the number of milliseconds that have elapsed since the SDL library initialization */
     const Uint64 now = SDL_GetTicks();
 
@@ -230,5 +291,15 @@ SDL_AppResult SDL_AppIterate(void *appstate)
 /* This function runs once at shutdown. */
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
+    int i;
+
+    for (i = 0; i < SDL_arraysize(sounds); i++) {
+        /* If less than a full copy of the audio is queued for playback, put another copy in there.
+           This is overkill, but easy when lots of RAM is cheap. One could be more careful and
+           queue less at a time, as long as the stream doesn't run dry.  */
+        if (SDL_GetAudioStreamQueued(sounds[i].stream) < ((int) sounds[i].wav_data_len)) {
+            SDL_PutAudioStreamData(sounds[i].stream, sounds[i].wav_data, (int) sounds[i].wav_data_len);
+        }
+    }
     /* SDL will clean up the window/renderer for us. */
 }
