c54414c6bbd138e3604d54cc1e0c9969fc95b4d6
gunzblazincourier <gunzblazinAKM@proton.me>
2026-01-12 21:56:14 -0700

    Adjust window and viewport resolution to ensure proper scaling, and update colors

--- pong.cpp
+++ pong.cpp
@@ -12,6 +12,7 @@
 #include <iostream>
 
 static bool display_menu = true;
+static bool display_options = false;
 enum Menu {PLAY = 0, OPTIONS = 1, QUIT = 2};
 static Menu menu_choice = PLAY;
 
@@ -22,8 +23,10 @@ static SDL_Renderer *renderer = NULL;
 static SDL_AudioDeviceID audio_device = 0;
 static Uint64 last_time = 0;    // The time deltatime before the current frame
 
-static const int WINDOW_WIDTH = 640;
-static const int WINDOW_HEIGHT = 480;
+static int WINDOW_WIDTH = 1920;
+static int WINDOW_HEIGHT = 1080;
+static const int GAME_WIDTH = 640;
+static const int GAME_HEIGHT = 480;
 
 enum Directions {UP = 1, DOWN = -1, ZERO = 0};
 static Directions s_direction_player = ZERO;
@@ -36,8 +39,8 @@ static float s_position_player_y = 100;
 static float s_position_cpu_y = 100;
 
 // Place ball in middle of screen
-static float s_position_ball_x = WINDOW_WIDTH/2;
-static float s_position_ball_y = WINDOW_HEIGHT/2;
+static float s_position_ball_x = GAME_WIDTH/2;
+static float s_position_ball_y = GAME_HEIGHT/2;
 
 // Set a random x (and by extension random y) between 0.0 and 1.0
 static float s_component_ball_x = SDL_randf();
@@ -93,12 +96,12 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("pong", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+    if (!SDL_CreateWindowAndRenderer("pong", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_FULLSCREEN, &window, &renderer)) {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
     /* Set a device-independent resolution and presentation mode for rendering. */
-    SDL_SetRenderLogicalPresentation(renderer, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_LOGICAL_PRESENTATION_INTEGER_SCALE);
+    SDL_SetRenderLogicalPresentation(renderer, GAME_WIDTH, GAME_HEIGHT, SDL_LOGICAL_PRESENTATION_INTEGER_SCALE);
 
     /* open the default audio device in whatever format it prefers; our audio streams will adjust to it. */
     audio_device = SDL_OpenAudioDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, NULL);
@@ -148,7 +151,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
 /* This function runs when a new event (mouse input, keypresses, etc) occurs. */
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
-    if (display_menu == false) {
+    if (display_menu == false && display_options == false) {
         // For when key is pressed
         if (event->type == SDL_EVENT_KEY_DOWN) {
             // Up key pressed
@@ -170,7 +173,7 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
                 s_direction_player = ZERO;
             }
         }
-    } else {
+    } else if (display_menu == true && display_options == false){
         if (event->type == SDL_EVENT_KEY_DOWN && event->key.repeat == false) {
             // Up key pressed
             if (event->key.scancode == SDL_SCANCODE_DOWN) {
@@ -208,44 +211,47 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 /* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    std::cout << menu_choice << std::endl;
-    /* as you can see from this, rendering draws over whatever was drawn before it. */
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);  /* black, full alpha */
-    SDL_RenderClear(renderer);  /* start with a blank canvas. */
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);  /* white, full alpha */
-
     /* Get the number of milliseconds that have elapsed since the SDL library initialization */
     const Uint64 now = SDL_GetTicks();
     const float deltatime = ((float) (now - last_time)) / 1000.0f;
-    if (display_menu == true) {
+
+    if (display_menu == true && display_options == false) {
+        SDL_SetRenderDrawColor(renderer, 0, 32, 63, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 173, 239, 209, SDL_ALPHA_OPAQUE);
+
         SDL_SetRenderScale(renderer, 4.0f, 4.0f);
-        SDL_RenderDebugText(renderer, WINDOW_WIDTH/12, WINDOW_HEIGHT/15, "PONG");
+        SDL_RenderDebugText(renderer, GAME_WIDTH/12, GAME_HEIGHT/15, "PONG");
 
         SDL_SetRenderScale(renderer, 2.0f, 2.0f);
         if (menu_choice == PLAY) {
-            SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
-            SDL_RenderDebugText(renderer, 2*WINDOW_WIDTH/10, 3*WINDOW_HEIGHT/15, "PLAY");
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-            SDL_RenderDebugText(renderer, 2*WINDOW_WIDTH/10, 3*WINDOW_HEIGHT/15+15, "OPTIONS");
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-            SDL_RenderDebugText(renderer, 2*WINDOW_WIDTH/10, 3*WINDOW_HEIGHT/15+30, "QUIT");
+            SDL_SetRenderDrawColor(renderer, 214, 237, 23, SDL_ALPHA_OPAQUE);
+            SDL_RenderDebugText(renderer, GAME_WIDTH/5, GAME_HEIGHT/5, "PLAY");
+            SDL_SetRenderDrawColor(renderer, 173, 239, 209, SDL_ALPHA_OPAQUE);
+            SDL_RenderDebugText(renderer, GAME_WIDTH/5, GAME_HEIGHT/5+15, "OPTIONS");
+            SDL_SetRenderDrawColor(renderer, 173, 239, 209, SDL_ALPHA_OPAQUE);
+            SDL_RenderDebugText(renderer, GAME_WIDTH/5, GAME_HEIGHT/5+30, "QUIT");
         } else if (menu_choice == OPTIONS) {
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-            SDL_RenderDebugText(renderer, 2*WINDOW_WIDTH/10, 3*WINDOW_HEIGHT/15, "PLAY");
-            SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
-            SDL_RenderDebugText(renderer, 2*WINDOW_WIDTH/10, 3*WINDOW_HEIGHT/15+15, "OPTIONS");
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-            SDL_RenderDebugText(renderer, 2*WINDOW_WIDTH/10, 3*WINDOW_HEIGHT/15+30, "QUIT");
+            SDL_SetRenderDrawColor(renderer, 173, 239, 209, SDL_ALPHA_OPAQUE);
+            SDL_RenderDebugText(renderer, GAME_WIDTH/5, GAME_HEIGHT/5, "PLAY");
+            SDL_SetRenderDrawColor(renderer, 214, 237, 23, SDL_ALPHA_OPAQUE);
+            SDL_RenderDebugText(renderer, GAME_WIDTH/5, GAME_HEIGHT/5+15, "OPTIONS");
+            SDL_SetRenderDrawColor(renderer, 173, 239, 209, SDL_ALPHA_OPAQUE);
+            SDL_RenderDebugText(renderer, GAME_WIDTH/5, GAME_HEIGHT/5+30, "QUIT");
         } else if (menu_choice == QUIT) {
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-            SDL_RenderDebugText(renderer, 2*WINDOW_WIDTH/10, 3*WINDOW_HEIGHT/15, "PLAY");
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-            SDL_RenderDebugText(renderer, 2*WINDOW_WIDTH/10, 3*WINDOW_HEIGHT/15+15, "OPTIONS");
-            SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
-            SDL_RenderDebugText(renderer, 2*WINDOW_WIDTH/10, 3*WINDOW_HEIGHT/15+30, "QUIT");
+            SDL_SetRenderDrawColor(renderer, 173, 239, 209, SDL_ALPHA_OPAQUE);
+            SDL_RenderDebugText(renderer, GAME_WIDTH/5, GAME_HEIGHT/5, "PLAY");
+            SDL_SetRenderDrawColor(renderer, 173, 239, 209, SDL_ALPHA_OPAQUE);
+            SDL_RenderDebugText(renderer, GAME_WIDTH/5, GAME_HEIGHT/5+15, "OPTIONS");
+            SDL_SetRenderDrawColor(renderer, 214, 237, 23, SDL_ALPHA_OPAQUE);
+            SDL_RenderDebugText(renderer, 2*GAME_WIDTH/10, GAME_HEIGHT/5+30, "QUIT");
         }
-        // SDL_RenderDebugTextFormat(renderer, 3*WINDOW_WIDTH/4, 100, "%d", s_score_cpu);
-    } else {
+        // SDL_RenderDebugTextFormat(renderer, 3*GAME_WIDTH/4, 100, "%d", s_score_cpu);
+    } else if (display_menu == false && display_options == false){
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 242, 170, 76, SDL_ALPHA_OPAQUE);
+
         /* If less than a full copy of the audio is queued for playback, put another copy in there.
             This is overkill, but easy when lots of RAM is cheap. One could be more careful and
             queue less at a time, as long as the stream doesn't run dry.  */
@@ -253,19 +259,26 @@ SDL_AppResult SDL_AppIterate(void *appstate)
             SDL_PutAudioStreamData(sounds[0].stream, sounds[0].wav_data, (int) sounds[0].wav_data_len);
         }
 
+        SDL_FRect background;
         SDL_FRect paddle_player;
         SDL_FRect paddle_cpu;
         SDL_FRect ball;
 
+        // Background covering only viewport and not entire screen
+        background.x = 0;
+        background.y = 0;
+        background.w = GAME_WIDTH;
+        background.h = GAME_HEIGHT;
+
         // Left edge of screen
-        paddle_player.x = 10;
+        paddle_player.x = 5;
         paddle_player.y = s_position_player_y;
         paddle_player.w = 10;
         paddle_player.h = 60;
 
 
         // Right edge of screen
-        paddle_cpu.x = WINDOW_WIDTH - 20;
+        paddle_cpu.x = GAME_WIDTH - 15;
         paddle_cpu.y = s_position_cpu_y;
         paddle_cpu.w = 10;
         paddle_cpu.h = 60;
@@ -279,8 +292,8 @@ SDL_AppResult SDL_AppIterate(void *appstate)
         // Bound player to screen if exceeding window limit, else move player paddle
         if (s_position_player_y < 0) {
             s_position_player_y = 0;
-        } else if (s_position_player_y > WINDOW_HEIGHT-paddle_player.h) {
-            s_position_player_y = WINDOW_HEIGHT-paddle_player.h;
+        } else if (s_position_player_y > GAME_HEIGHT-paddle_player.h) {
+            s_position_player_y = GAME_HEIGHT-paddle_player.h;
         } else {
             s_position_player_y -= 300*s_direction_player*deltatime;
         }
@@ -291,7 +304,7 @@ SDL_AppResult SDL_AppIterate(void *appstate)
             s_direction_cpu = DOWN;
         }
         
-        if (s_position_cpu_y > WINDOW_HEIGHT-paddle_cpu.h) {
+        if (s_position_cpu_y > GAME_HEIGHT-paddle_cpu.h) {
             s_direction_cpu = UP;
         }
 
@@ -339,42 +352,50 @@ SDL_AppResult SDL_AppIterate(void *appstate)
         if (s_position_ball_y <= 0) {
             s_direction_ball_y = DOWN;
         }
-        if (s_position_ball_y >= WINDOW_HEIGHT) {
+        if (s_position_ball_y >= GAME_HEIGHT) {
             s_direction_ball_y = UP;
         }
 
         // When ball crosses left or right side of screen and someone scores
         if (s_position_ball_x < -20) {
-            s_position_ball_x = WINDOW_WIDTH/2;
-            s_position_ball_y = SDL_rand(WINDOW_HEIGHT);
+            s_position_ball_x = GAME_WIDTH/2;
+            s_position_ball_y = SDL_rand(GAME_HEIGHT);
             s_component_ball_x = SDL_randf();
             s_direction_ball_x = UP;
             SDL_PutAudioStreamData(sounds[1].stream, sounds[1].wav_data, (int) sounds[1].wav_data_len);
             s_score_cpu++;
         }
 
-        if (s_position_ball_x > WINDOW_WIDTH + 20) {
-            s_position_ball_x = WINDOW_WIDTH/2;
-            s_position_ball_y = WINDOW_HEIGHT/2;
+        if (s_position_ball_x > GAME_WIDTH + 10) {
+            s_position_ball_x = GAME_WIDTH/2;
+            s_position_ball_y = SDL_rand(GAME_HEIGHT);
             s_component_ball_x = SDL_randf();
             s_direction_ball_x = UP;
             SDL_PutAudioStreamData(sounds[1].stream, sounds[1].wav_data, (int) sounds[1].wav_data_len);
             s_score_player++;
         }
 
+        SDL_SetRenderDrawColor(renderer, 16, 24, 32, SDL_ALPHA_OPAQUE);
+        SDL_RenderFillRect(renderer, &background);
+
         // Rendering paddles and ball
+        SDL_SetRenderDrawColor(renderer, 242, 170, 76, SDL_ALPHA_OPAQUE);
         SDL_RenderFillRect(renderer, &paddle_player);
         SDL_RenderFillRect(renderer, &paddle_cpu);
+
+        SDL_SetRenderDrawColor(renderer, 233, 75, 60, SDL_ALPHA_OPAQUE);
         SDL_RenderFillRect(renderer, &ball);
 
         // Display scores
         SDL_SetRenderScale(renderer, 1.0f, 1.0f);
-        SDL_RenderDebugTextFormat(renderer, WINDOW_WIDTH/4, 100, "%d", s_score_player);
-        SDL_RenderDebugTextFormat(renderer, 3*WINDOW_WIDTH/4, 100, "%d", s_score_cpu);
+        SDL_SetRenderDrawColor(renderer, 255, 214, 98, SDL_ALPHA_OPAQUE);
+        SDL_RenderDebugTextFormat(renderer, GAME_WIDTH/4, 100, "%d", s_score_player);
+        SDL_RenderDebugTextFormat(renderer, 3*GAME_WIDTH/4, 100, "%d", s_score_cpu);
 
         // Middle partition
-        for (int i = 0; i < WINDOW_HEIGHT; i += 10) {
-            SDL_RenderPoint(renderer, WINDOW_WIDTH/2, i);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+        for (int i = 0; i < GAME_HEIGHT; i += 10) {
+            SDL_RenderPoint(renderer, GAME_WIDTH/2, i);
         }
     }
 
